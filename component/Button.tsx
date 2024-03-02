706552b1709555b2767a8abb27a8c83a42cc1f65
HoangDuong2124 <103102495+HoangDuong2124@users.noreply.github.com>
2024-03-02 16:55:49 +0700

    update component

--- component/Button.tsx
+++ component/Button.tsx
@@ -1,47 +1,33 @@
-export const ButtonSidebar = ({path,name}:{path:string,name:string}) =>{
-    return(
-        <button className="w-full flex items-center rounded-[5px] p-2 hover:bg-slate-200">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 512 512"
-          width={13}
-          height={13}
-        >
-          <path
-            className="fill-slate-500"
-            d={path}
-          />
-        </svg>
-        <div className="ml-[14px] text-[14px] text-slate-500 leading-[21px] font-[500]">
-          {name}
-        </div>
-      </button>
-    )
-}
+import Image from "next/image";
+import { FileSVG, InboxSVG, PlusSVG, SearchSVG, SettingSVG, ThreeDotSVG } from "./svg";
+export const ButtonSidebar = ({
+  path:IconComponent,
+  name,
+}: {
+  path: React.ComponentType;
+  name: string;
+}) => {
+  return (
+    <button className="w-full flex items-center rounded-[5px] p-2 hover:bg-slate-200">
+      {IconComponent && <IconComponent />} 
+      <div className="ml-[14px] text-[14px] text-slate-500 leading-[21px] font-[500]">
+        {name}
+      </div>
+    </button>
+  );
+};
 
-export const ButtonNote = () =>{
-    return(
-        <button className="group-hover:group-hover w-full flex  items-center rounded-[5px]  p-2 hover:bg-slate-200 relative">
-        <svg
-          role="graphics-symbol"
-          viewBox="0 0 16 16"
-          className="w-[18px] h-[18px] block fill-slate-500 "
-        >
-          <path d="M4.35645 15.4678H11.6367C13.0996 15.4678 13.8584 14.6953 13.8584 13.2256V7.02539C13.8584 6.0752 13.7354 5.6377 13.1406 5.03613L9.55176 1.38574C8.97754 0.804688 8.50586 0.667969 7.65137 0.667969H4.35645C2.89355 0.667969 2.13477 1.44043 2.13477 2.91016V13.2256C2.13477 14.7021 2.89355 15.4678 4.35645 15.4678ZM4.46582 14.1279C3.80273 14.1279 3.47461 13.7793 3.47461 13.1436V2.99219C3.47461 2.36328 3.80273 2.00781 4.46582 2.00781H7.37793V5.75391C7.37793 6.73145 7.86328 7.20312 8.83398 7.20312H12.5186V13.1436C12.5186 13.7793 12.1836 14.1279 11.5205 14.1279H4.46582ZM8.95703 6.02734C8.67676 6.02734 8.56055 5.9043 8.56055 5.62402V2.19238L12.334 6.02734H8.95703ZM10.4336 9.00098H5.42969C5.16992 9.00098 4.98535 9.19238 4.98535 9.43164C4.98535 9.67773 5.16992 9.86914 5.42969 9.86914H10.4336C10.6797 9.86914 10.8643 9.67773 10.8643 9.43164C10.8643 9.19238 10.6797 9.00098 10.4336 9.00098ZM10.4336 11.2979H5.42969C5.16992 11.2979 4.98535 11.4893 4.98535 11.7354C4.98535 11.9746 5.16992 12.1592 5.42969 12.1592H10.4336C10.6797 12.1592 10.8643 11.9746 10.8643 11.7354C10.8643 11.4893 10.6797 11.2979 10.4336 11.2979Z"></path>
-        </svg>
-        <p className="ml-2 text-[14px] text-black font-[500]">Untitled</p>
-        <div
-          title="Delete, Rename, and more..."
-          className="w-[25px] h-[20px] flex justify-center items-center  rounded-[5px]  hover:bg-slate-300 absolute right-2"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 448 512"
-            className="w-[15px] h-[15px] fill-slate-500 opacity-0 group-hover:opacity-100"
-          >
-            <path d="M8 256a56 56 0 1 1 112 0A56 56 0 1 1 8 256zm160 0a56 56 0 1 1 112 0 56 56 0 1 1 -112 0zm216-56a56 56 0 1 1 0 112 56 56 0 1 1 0-112z" />
-          </svg>
-        </div>
-      </button>
-    )
-}
+export const ButtonNote = () => {
+  return (
+    <button className="group-hover:group-hover w-full flex  items-center rounded-[5px]  p-2 hover:bg-slate-200 relative">
+      <FileSVG />
+      <p className="ml-2 text-[14px] text-black font-[500]">Untitled</p>
+      <div
+        title="Delete, Rename, and more..."
+        className="w-[25px] h-[20px] flex justify-center items-center  rounded-[5px]  hover:bg-slate-300 absolute right-2"
+      >
+        <ThreeDotSVG />
+      </div>
+    </button>
+  );
+};
--- component/Sidebar.tsx
+++ component/Sidebar.tsx
@@ -1,4 +1,15 @@
 import { ButtonNote, ButtonSidebar } from "./Button";
+import {
+  Add,
+  AnglesBottomSVG,
+  AnglesLeftSVG,
+  AnglesTopSVG,
+  InboxSVG,
+  Plus,
+  PlusSVG,
+  SearchSVG,
+  SettingSVG,
+} from "./svg";
 
 export const SidebarLayout = () => {
   return (
@@ -11,28 +22,8 @@ export const SidebarLayout = () => {
             </div>
             <div className="mx-2">Traning</div>
             <div>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 512 512"
-                width={7}
-                height={7}
-              >
-                <path
-                  className="fill-slate-500"
-                  d="M233.4 105.4c12.5-12.5 32.8-12.5 45.3 0l192 192c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L256 173.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l192-192z"
-                />
-              </svg>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 512 512"
-                width={7}
-                height={7}
-              >
-                <path
-                  className="fill-slate-500"
-                  d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z"
-                />
-              </svg>
+              <AnglesTopSVG />
+              <AnglesBottomSVG />
             </div>
           </div>
 
@@ -40,58 +31,17 @@ export const SidebarLayout = () => {
             title="Close Sidebar"
             className="w-[25px] h-[25px] rounded-[5px] flex justify-center items-center group-hover:opacity-100 hover:bg-slate-300 opacity-0"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 320 512"
-              width={8}
-              height={11}
-            >
-              <path
-                className="fill-slate-500"
-                d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"
-              />
-            </svg>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 320 512"
-              width={8}
-              height={11}
-            >
-              <path
-                className="fill-slate-500"
-                d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"
-              />
-            </svg>
+            <AnglesLeftSVG />
           </div>
         </button>
-        <ButtonSidebar
-          path="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"
-          name="Search"
-        />
 
-        <ButtonSidebar
-          path="M121 32C91.6 32 66 52 58.9 80.5L1.9 308.4C.6 313.5 0 318.7 0 323.9V416c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V323.9c0-5.2-.6-10.4-1.9-15.5l-57-227.9C446 52 420.4 32 391 32H121zm0 64H391l48 192H387.8c-12.1 0-23.2 6.8-28.6 17.7l-14.3 28.6c-5.4 10.8-16.5 17.7-28.6 17.7H195.8c-12.1 0-23.2-6.8-28.6-17.7l-14.3-28.6c-5.4-10.8-16.5-17.7-28.6-17.7H73L121 96z"
-          name="Inbox"
-        />
-
-        <ButtonSidebar
-          path="M495.9 166.6c3.2 8.7 .5 18.4-6.4 24.6l-43.3 39.4c1.1 8.3 1.7 16.8 1.7 25.4s-.6 17.1-1.7 25.4l43.3 39.4c6.9 6.2 9.6 15.9 6.4 24.6c-4.4 11.9-9.7 23.3-15.8 34.3l-4.7 8.1c-6.6 11-14 21.4-22.1 31.2c-5.9 7.2-15.7 9.6-24.5 6.8l-55.7-17.7c-13.4 10.3-28.2 18.9-44 25.4l-12.5 57.1c-2 9.1-9 16.3-18.2 17.8c-13.8 2.3-28 3.5-42.5 3.5s-28.7-1.2-42.5-3.5c-9.2-1.5-16.2-8.7-18.2-17.8l-12.5-57.1c-15.8-6.5-30.6-15.1-44-25.4L83.1 425.9c-8.8 2.8-18.6 .3-24.5-6.8c-8.1-9.8-15.5-20.2-22.1-31.2l-4.7-8.1c-6.1-11-11.4-22.4-15.8-34.3c-3.2-8.7-.5-18.4 6.4-24.6l43.3-39.4C64.6 273.1 64 264.6 64 256s.6-17.1 1.7-25.4L22.4 191.2c-6.9-6.2-9.6-15.9-6.4-24.6c4.4-11.9 9.7-23.3 15.8-34.3l4.7-8.1c6.6-11 14-21.4 22.1-31.2c5.9-7.2 15.7-9.6 24.5-6.8l55.7 17.7c13.4-10.3 28.2-18.9 44-25.4l12.5-57.1c2-9.1 9-16.3 18.2-17.8C227.3 1.2 241.5 0 256 0s28.7 1.2 42.5 3.5c9.2 1.5 16.2 8.7 18.2 17.8l12.5 57.1c15.8 6.5 30.6 15.1 44 25.4l55.7-17.7c8.8-2.8 18.6-.3 24.5 6.8c8.1 9.8 15.5 20.2 22.1 31.2l4.7 8.1c6.1 11 11.4 22.4 15.8 34.3zM256 336a80 80 0 1 0 0-160 80 80 0 1 0 0 160z"
-          name="Setting & members"
-        />
+        <ButtonSidebar path={SearchSVG} name="Search" />
+        <ButtonSidebar path={InboxSVG} name="Inbox" />
+        <ButtonSidebar path={SettingSVG} name="Setting & members" />
 
         <button className=" w-full flex  items-center rounded-[5px] p-2 hover:bg-slate-200">
           <div className="w-[15px] h-[15px] rounded-full bg-slate-400  flex justify-center items-center ">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 448 512"
-              width={8}
-              height={12}
-            >
-              <path
-                className="fill-white"
-                d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"
-              />
-            </svg>
+            <Add />
           </div>
           <div className="ml-[14px] text-[14px] text-slate-500 leading-[21px] font-[500]">
             New page
@@ -108,24 +58,11 @@ export const SidebarLayout = () => {
             title="Add a page"
             className="w-[18px] h-[18px] flex justify-center items-center hover:bg-slate-300 rounded-[3px] "
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 448 512"
-              width={10}
-              height={12}
-            >
-              <path
-                className="fill-slate-500 opacity-0 group-hover:opacity-100 "
-                d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"
-              />
-            </svg>
+            <PlusSVG />
           </button>
         </div>
 
-        <ButtonSidebar
-          path="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"
-          name="Add a page"
-        />
+        <ButtonSidebar path={Plus} name="Add a page" />
 
         <ButtonNote />
       </div>
